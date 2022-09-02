c2da009c2514042f6a2a8f708ff765a7c63ddc36
vavryshchuk <112618316+vavryshchuk@users.noreply.github.com>
2022-09-02 15:40:40 +0300

    Update PrintSentenceApp.java

--- src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
+++ src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task1;
 
 public class PrintSentenceApp {
     public static void main(String[] args) {
+        System.out.println("java");
+        System.out.println("is");
+        System.out.println("smth");
     }
 }
