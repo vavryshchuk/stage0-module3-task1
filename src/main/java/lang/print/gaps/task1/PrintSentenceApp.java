12e6b0dd72543577f608c2bd51a823809b273732
vavryshchuk <112618316+vavryshchuk@users.noreply.github.com>
2022-09-02 16:16:45 +0300

    Update PrintSentenceApp.java

--- src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
+++ src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
@@ -2,10 +2,7 @@ package lang.print.gaps.task1;
 
 public class PrintSentenceApp {
     public static void main(String[] args) {
-        System.out.println("This");
-        System.out.println("is");
-        System.out.println("my");
-        System.out.println("Java");
-        System.out.println("program");
+        System.out.println("This is my first Java program");
+        
     }
 }
