ba58892e5bc65462eb2fe71b7ef068ba511c1520
vavryshchuk <112618316+vavryshchuk@users.noreply.github.com>
2022-09-02 15:55:04 +0300

    Update UpdatedApp.java

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -2,6 +2,16 @@ package lang.print.gaps.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.println("fixMe");
+        System.out.println("I'm");
+        System.out.println("updated");
+        System.out.println(" ");
+        System.out.println("can");
+        System.out.println("be");
+        System.out.println("compiled.");
+        System.out.println("And");
+        System.out.println("can");
+        System.out.println("write");
+        System.out.println("to");
+        System.out.println("console");
     }
 }
